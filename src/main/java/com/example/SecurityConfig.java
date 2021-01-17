f52fe564aab5d4b59c7f05da2f230ce8d7b186e2
tac-yacht <tac-yacht@users.noreply.github.com>
2021-01-17 10:33:02 +0900

    グループごとの認証評価を追加

--- src/main/java/com/example/SecurityConfig.java
+++ src/main/java/com/example/SecurityConfig.java
@@ -12,8 +12,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.authorizeRequests()
 				.mvcMatchers("/").authenticated()
 				.mvcMatchers("/itemnew", "/item/**").authenticated()
-				.mvcMatchers("/group/{groupId}/**").access("isAuthenticated() and @groupPermission.hasAuthority(principal, #groupId)")
-				//先行評価で認証済みでないと、principal取るときに死ぬ特段ログインに回してはくれない
+				.mvcMatchers("/group/{groupId}/**").access("isAuthenticated() and @groupPermission.hasAuthority(principal, #groupId, 'ADMIN')")
+				//accsess内の短絡評価で先に認証済みでないと、principal取ろうとすると死ぬ。特段ログインに回してはくれない
+				//使用先メソッド側でOptionalにしても受け取れるけど、認証済みでないと落ちる。
+				//今回は独自実装のhasAuthorityの引数に使うので、先に認証評価をする。
 				.mvcMatchers("/admin/**").hasRole("ADMIN")
 				.anyRequest().denyAll()
 			.and()
--- src/main/java/com/example/security/GroupPermission.java
+++ src/main/java/com/example/security/GroupPermission.java
@@ -1,7 +1,12 @@
 package com.example.security;
 
+import java.util.Collection;
+import java.util.Set;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,10 +15,52 @@ public class GroupPermission {
 	/** ロガー */
 	private static final Logger log = LoggerFactory.getLogger(GroupPermission.class);
 
-	public boolean hasAuthority(CustomUser user, String groupId) {
-//		auth.getPrincipal()
-		log.info("user: {}", user);
-		log.info("groupId: {}", groupId);
-		return true;
+	public boolean hasAuthority(CustomUser user, String groupId, String authority) {
+		return hasAuthority(user.getSingleGroupAuthorities(groupId), authority);
+	}
+
+	//ここから↓SecurityExpressionRootを参考に改造
+
+	//セットだけ持ち回すように改造
+	public final boolean hasAuthority(Collection<GrantedAuthority> target, String authority) {
+		return hasAnyAuthority(target, authority);
+	}
+
+	//セットだけ持ち回すように改造
+	public final boolean hasAnyAuthority(Collection<GrantedAuthority> target, String ... authorities) {
+		return hasAnyAuthorityName(target, null, authorities);
+	}
+
+	//セットだけ持ち回すように改造
+	private boolean hasAnyAuthorityName(Collection<GrantedAuthority> target, String prefix, String... roles) {
+		Set<String> roleSet = getAuthoritySet(target);
+
+		for (String role : roles) {
+			String defaultedRole = getRoleWithDefaultPrefix(prefix, role);
+			if (roleSet.contains(defaultedRole)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	//がっつり改造
+	private Set<String> getAuthoritySet(Collection<GrantedAuthority> target) {
+		return AuthorityUtils.authorityListToSet(target);
+	}
+
+	//丸ごとコピー
+	private static String getRoleWithDefaultPrefix(String defaultRolePrefix, String role) {
+		if (role == null) {
+			return role;
+		}
+		if (defaultRolePrefix == null || defaultRolePrefix.length() == 0) {
+			return role;
+		}
+		if (role.startsWith(defaultRolePrefix)) {
+			return role;
+		}
+		return defaultRolePrefix + role;
 	}
 }
