6faadaada54cc0e6f957b39fdc95c4e8246c6b33
tac-yacht <tac-yacht@users.noreply.github.com>
2021-01-17 19:09:21 +0900

    モデルにグループID追加

--- src/main/java/com/example/GroupController.java
+++ src/main/java/com/example/GroupController.java
@@ -2,11 +2,18 @@ package com.example;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/group/{groupId}")
 public class GroupController {
+	@ModelAttribute("groupId")
+	public String setItem(@PathVariable("groupId") String groupId) {
+		return groupId;
+	}
+
 	@GetMapping
 	public String view() {
 		return "group/index";
